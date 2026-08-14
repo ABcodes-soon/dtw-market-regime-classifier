d33d57beea28b7d003f196a14776f8d962c62c20
ABcodes-soon <ABcodes-soon@users.noreply.github.com>
2026-08-14 18:37:51 -0400

    mini_03_cluster

--- mini_projects/mini_03_cluster.py
+++ mini_projects/mini_03_cluster.py
@@ -2,3 +2,20 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from tslearn.clustering import TimeSeriesKMeans
+from tslearn.metrics import cdist_dtw
+from sklearn.metrics import silhouette_score
+
+
+# Use yfinance to download historical stock data for a list of tickers
+tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "NVDA", "JPM", "V", "JNJ"]
+
+print("Downloading historical stock data...")
+
+prices_data = yf.download(tickers, start = "2015-01-01", end = "2024-12-31", auto_adjust = True)
+
+current_prices = prices_data["Close"].dropna(axis = 1, how = 'all')
+
+
+
+
+
