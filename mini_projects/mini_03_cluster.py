0cf3e501df2b60b9035a02c719f223eef735e98a
ABcodes-soon <ABcodes-soon@users.noreply.github.com>
2026-08-14 20:06:03 -0400

    mini cluster 3 DTW matrix and silhoutte matrix done

--- mini_projects/mini_03_cluster.py
+++ mini_projects/mini_03_cluster.py
@@ -15,7 +15,27 @@ prices_data = yf.download(tickers, start = "2015-01-01", end = "2024-12-31", aut
 
 current_prices = prices_data["Close"].dropna(axis = 1, how = 'all')
 
+#### Part 2:
 
+returns = np.log(current_prices / current_prices.shift(1)).dropna()
+print(f"Return shape: {returns.shape}")
+# result sin 2514 rows and 10 columns, which means we have 2514 daily returns for each of the 10 tickers.
+
+
+# Prepare for tslearn: (nlearns, n_timestamp)
+returns_array = returns.T.values  # Transpose to have shape (n_tickers, n_days)
+# tslearn expects: (n_series, n_timestamps) — each row is a ticker, each column is a day.
+print(f"Array shape for tslearn: {returns_array.shape}")
+
+# DTW Matrix + silhouette loop
+
+
+#Term	What It Means in Plain English
+# cdist_dtw	Cross-Distance with Dynamic Time Warping — computes the distance between every pair of time series
+# returns_array	Your stock returns: shape is (10 tickers, 2265 days)
+# dtw_dist	A 10×10 matrix where each cell is the DTW distance between two stocks
+
+dtw_dist = cdist_dtw(returns_array)
 
 
 
